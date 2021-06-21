fdf00ae31a0a61b77eff729f20310a34cb457734
thesc <thescrill12@gmail.com>
2021-06-21 16:21:29 -0400

    Electron app

---
+++ my-electron-app/main.js
@@ -0,0 +1,27 @@
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+//function that loads index.html into a new BrowserWindow instance
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    })
+
+    win.loadFile('index.html')
+}
+
+// call this createWindow() function to open your window.
+app.whenReady().then(() => {
+    createWindow()
+//Do this by attaching your event listener from within your existing whenReady() callback.
+    app.on('activate', function () {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit()
+})
