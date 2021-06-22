199ff82168edcd79bd0a38f01f7f7640f4cdc286
thesc <thescrill12@gmail.com>
2021-06-22 11:16:51 -0400

    Check in

--- my-electron-app/main.js
+++ my-electron-app/main.js
@@ -4,13 +4,13 @@ const path = require('path');
 //Function that loads index.html into a new BrowserWindow instance
 const {app, BrowserWindow} = electron;
 
-let mainWindow;
+let index;
 
 //Listen for app to be ready
 app.on('ready', function () {
-    mainWindow = new BrowserWindow({});
+    index = new BrowserWindow({});
 //This code is passing this path file://dirname/index.html
-    mainWindow.loadURL(url.format({
+    index.loadURL(url.format({
         pathname: path.join(__dirname, 'index.html'),
         protocol: 'file:',
         slashes: true
