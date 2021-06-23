25574edd88638c39808474d22a1d0df2b5280fa2
thesc <thescrill12@gmail.com>
2021-06-23 17:25:27 -0400

    Check on the render thread

--- my-electron-app/renderer/index.js
+++ my-electron-app/renderer/index.js
@@ -3,8 +3,8 @@ const ipc = electron.ipcMain
 
 const messageIn = document.getElementById('message');
 
-messageIn.addEventListener('click', function (){
-    ipc.send('open-message');
+messageIn.addEventListener('click', function (channel, listener){
+    ipc.handle('open-message', listener);
 })
 ipc.on('open-message', function (event, arg) {
     console.log(arg);
