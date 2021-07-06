acc1164987ba88f8735a5bdc16a07fc2bb3c0ae2
thesc <thescrill12@gmail.com>
2021-07-06 13:55:01 -0400

    Fetch JS

--- my-electron-app/renderer/index.js
+++ my-electron-app/renderer/index.js
@@ -31,3 +31,13 @@ window.addEventListener('click', (event) => {
     }
 });
 
+async function postData(url = '', data = {}) {
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+    return response.json();
+}
